3d4a481d73ca1f847c948e5c858f3fdd0bcd391d
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-04-28 21:51:03 +0545

    test changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="text-gray-600 body-font fixed w-full top-0 bg-white h-20">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-          <a className="mr-5 hover:text-gray-900">First Link</a>
+          <a className="mr-5 hover:text-gray-900">First Lnlcnalink</a>
           <a className="mr-5 hover:text-gray-900">Second Link</a>
           <a className="mr-5 hover:text-gray-900">Third Link</a>
           <a className="hover:text-gray-900">Fourth Link</a>
